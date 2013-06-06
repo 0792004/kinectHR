0bc78a4cb1d69644df3d732c265107c8052701e2
0792008 <joong555@naver.com>
2013-06-06 14:45:22 +0900

    rm

--- KinectBridgeWithOpenCVBasics-D2D/targetver.h
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------------------------
-// <copyright file="targetver.h" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
-#pragma once
-
-#include <SDKDDKVer.h>
